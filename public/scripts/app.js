8bac793eaac1ffec8aa9049848e141551b3d183f
jswelsh <james.sinclair.welsh@gmail.com>
2019-07-23 01:48:08 +0000

    fixed cross site scripting, the code is clearer now

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,48 +3,46 @@ $(document).ready(()=>{
 
   const validTweetValidator = function(tweetContents) {
     if (tweetContents === "") {
-      //$('.warning').css('visibility', 'visible');
       $('.warning').slideDown("slow");
-      //alert("Your tweet has no chirp, flex more chirp!");
       $('.warning').text('Your tweet has no chirp, flex more chirp!');
       return false;
     } else if ((tweetContents.length) > 140) {
-      //$('.warning').css('visibility', 'visible');
       $('.warning').slideDown("slow");
-      //alert("Your tweet has no chirp, flex more chirp!");
       $('.warning').text('Your tweet has to much chirp, flex less chirp!');
-      //alert("Your tweet has to much chirp, flex less chirp!");
       return false;
     }
     $('.warning').slideUp("slow");
     return true;
   };
-  
+  //escapes input that can cause crosssite scripting
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
   const renderTweets = function(tweets) {
     // loops through tweets
     tweets.forEach(function(element) {
       let $tweet = createTweetElement(element);
       $('.tweets').append($tweet);
     });
-  
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
   };
-  
+  //generates new tweet markup
   const createTweetElement = function(tweet) {
     let $tweet = $('<article>').addClass('tweetBox');
-   // let tweetText = (tweet.content.text);
+    let safeText = escape(tweet.content.text)    
     let tweetArticle =
   `
     <header>
-    <h2> ${tweet.user.name} </h2> 
+      <h2> ${tweet.user.name} </h2> 
       <small class="hide" class="rightSide"> ${tweet.user.handle}</small>
       <img src= ${tweet.user.avatars}>
     </header>
-    <p>${tweet.content.text}</p>
+      <p>${safeText}</p>
     <footer>
-
-        <time>${tweet.content.created_at}</time>
+      <time>${tweet.content.created_at}</time>
       <small class="rightSide"> links </small>
     </footer>
     </article>`;
@@ -56,10 +54,9 @@ $(document).ready(()=>{
     $button.on('submit', (event) => {
       event.preventDefault();
       const tweet = (document.forms['tweetInputField']['tweeterText'].value);
-
       if (validTweetValidator(tweet)) {
         $.post('/tweets', $('.tweetSubmit').serialize(), (data) => {
-      
+          //incase if a new tweet is submitted
           loadTweets(1);
         });
       }
@@ -67,21 +64,13 @@ $(document).ready(()=>{
   });
 
   $(function() {
-    const $button =$(".newTweetButton");
-    
+    const $button =$(".newTweetButton"); 
     $button.click(function(){
       event.preventDefault();
       $(".new-tweet").slideToggle("slow");
   });
   });
   
-/*   const loadTweets = function(size) {
-    $.getJSON('http://localhost:8080/tweets', (tweetJSON) => {
-      renderTweets(tweetJSON);
-      //console.error(tweetJSON, "test");
-    });
-  };
-  loadTweets(); */
   function loadTweets(size){
     $.get('http://localhost:8080/tweets', (function (tweets) {
       if (size) {
@@ -92,7 +81,6 @@ $(document).ready(()=>{
     }));
   };
   loadTweets();
-
 });
 
 
