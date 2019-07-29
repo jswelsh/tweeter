be04f54e7c968e9d451bb7a4b8a058a39ae9c719
jswelsh <james.sinclair.welsh@gmail.com>
2019-07-29 08:32:52 +0000

    add comments, fix char counter format

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,30 +1,36 @@
-// Fake data taken from initial-tweets.json
-$(document).ready(()=>{
 
+$(document).ready(()=>{
+  //check each tweet submitted and ensures it does not exceed
+  //max character count nor is the tweet an empty field
   const validTweetValidator = function(tweetContents) {
     if (tweetContents === "") {
+      //push down warning message
       $('.warning').slideDown("slow");
       $('.warning').text('Your tweet has no chirp, flex more chirp!');
       return false;
     } else if ((tweetContents.length) > 140) {
+      //push down warning message
       $('.warning').slideDown("slow");
       $('.warning').text('Your tweet has to much chirp, flex less chirp!');
       return false;
     }
+    //return warning to original position upon
+    //proper input
     $('.warning').slideUp("slow");
     return true;
   };
-  //escapes input that can cause crosssite scripting
+  //xss sanatization of user input text
   const escape =  function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   }
   const renderTweets = function(tweets) {
-    // loops through tweets
+    // loops through tweets to render
     tweets.forEach(function(element) {
       let $tweet = createTweetElement(element);
-      $('.tweets').append($tweet);
+      //afix each new tweet to the the top of the the tweet section
+      $('.tweets').prepend($tweet);
     });
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
@@ -32,15 +38,14 @@ $(document).ready(()=>{
   //generates new tweet markup
   const createTweetElement = function(tweet) {
     let $tweet = $('<article>').addClass('tweetBox');
-    let safeText = escape(tweet.content.text)    
-    console.log(tweet);
+    //xss sanatization of user input text
+    let safeText = escape(tweet.content.text);
     let tweetArticle =
   `
   <small class="hide" class="rightSide"> ${tweet.user.handle}</small>
     <header>
       <h2> ${tweet.user.name} </h2> 
-    </header>
-      
+    </header>  
       <img src= ${tweet.user.avatars}>
       <p>${safeText}</p>
     <footer>
@@ -68,7 +73,7 @@ $(document).ready(()=>{
   $("button").click(function(){
     $(".new-tweet").toggle(1000);
   });
-
+  //grabs all stored tweets
   function loadTweets(size){
     $.get('http://localhost:8080/tweets', (function (tweets) {
       if (size) {
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,15 @@
-console.log ("composer counter checked and linked");
+
 $(document).ready(function() {
+  //character counting function, alerts user if
+  //they exceed the maximum allowable char count
+  //for tweet
   $('#tweeterText').keyup('click', function() {
     let maxLength = 140;
     $('#counter').html(maxLength)
     let textLength = $('#tweeterText').val().length;
     let remaining = maxLength - textLength;
-    if (remaining === 0) {
-      $('#counter').html('Max!');
+    if (remaining < 0) {
+      $('#counter').html(`<font color="red"> Max char: ${remaining}</font>`);
     } else {
     $('#counter').html(remaining);
     }
