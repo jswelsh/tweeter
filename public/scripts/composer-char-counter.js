6bfc093d3fe93132c4105f94aa6fb524616518bf
jswelsh <james.sinclair.welsh@gmail.com>
2020-06-12 16:27:36 -0700

    removed redundant variable that 'overrides' the html that's hardcoded, unnecessary. removed the old conditional, maybe go back to if/else? but probably not...idk man

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,23 +2,13 @@
 $(document).ready(function() {
   //character counting function, alerts user if
   //they exceed the maximum allowable char count
-  //for tweet
+  //for tweet as well as add color of overage
   $('#tweeterText').keyup('click', function() {
     const maxLength = 140;
-    $('#counter').html(maxLength);
     const textLength = $('#tweeterText').val().length;
     const remaining = maxLength - textLength;
-
-/*     if (remaining < 0) {
-      $('#counter').html(`<font color="red"> Max char: ${remaining}</font>`);
-    } else {
-      $('#counter').html(remaining);
-    } */
-/*     (remaining < 0) ?
-      $('#counter').html(`<font color="red"> Max char: ${remaining}</font>`) : 
-      $('#counter').html(remaining); */
-      $('#counter').html( (remaining < 0) ? `<font color="red"> Max char: ${remaining}</font>` : remaining )
-
+    $('#counter').html( 
+      (remaining < 0) ? `<font color="red"> Max char: ${remaining}</font>` : remaining 
+    )
   });
 });
- 
