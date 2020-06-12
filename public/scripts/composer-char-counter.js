fc6cb7a00c04a461c0d0fa70f0444e7c3192c465
jswelsh <james.sinclair.welsh@gmail.com>
2020-06-12 16:09:58 -0700

    changed a few -let- declarations to -const- and trying out ternary over if else as it is a bit easier to read

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,15 +4,21 @@ $(document).ready(function() {
   //they exceed the maximum allowable char count
   //for tweet
   $('#tweeterText').keyup('click', function() {
-    let maxLength = 140;
+    const maxLength = 140;
     $('#counter').html(maxLength);
-    let textLength = $('#tweeterText').val().length;
-    let remaining = maxLength - textLength;
-    if (remaining < 0) {
+    const textLength = $('#tweeterText').val().length;
+    const remaining = maxLength - textLength;
+
+/*     if (remaining < 0) {
       $('#counter').html(`<font color="red"> Max char: ${remaining}</font>`);
     } else {
       $('#counter').html(remaining);
-    }
+    } */
+/*     (remaining < 0) ?
+      $('#counter').html(`<font color="red"> Max char: ${remaining}</font>`) : 
+      $('#counter').html(remaining); */
+      $('#counter').html( (remaining < 0) ? `<font color="red"> Max char: ${remaining}</font>` : remaining )
+
   });
 });
  
